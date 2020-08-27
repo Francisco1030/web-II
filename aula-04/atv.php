5b8e094dd35440d52311483e6714b46387c949df
Francisco Viana <franciscoviana862@gmail.com>
2020-08-26 22:49:28 -0300

    texto

--- aula-04/atv.php
+++ aula-04/atv.php
@@ -1,4 +1,21 @@
 <?php
+/*
+Dado um array, desenvolva um programa PHP que: 
+
+a) Retorne todos os elementos do array que contenham a palavra PHP e tenham mais de três caracteres, ou seja, elementos que contenham apenas a palavra PHP não devem ser considerados. 
+
+b) Retorne o array original sem os elementos que satisfizeram o critério definido no item a. 
+
+Exemplo: 
+
+$_array = array ("PHP",1,10,"PHP5","PHP4", " Livro de PHP",2); 
+
+Resultado: 
+
+array("PHP5","PHP4","Livro de PHP")
+
+*/
+
 
 $_array = array("PHP", 1, 10, "PHP5", "PHP4", " Livro de PHP", 2);
 
